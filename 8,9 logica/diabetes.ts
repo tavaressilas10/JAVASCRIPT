e85f9dd0cf944f057a85b3eeec9cb2e9aec3829e
Silas Tavares <112710584+tavaressilas10@users.noreply.github.com>
2022-09-24 18:49:39 -0300

    js

---
+++ 8,9 logica/diabetes.ts
@@ -0,0 +1,18 @@
+let nome, peso, altura, datainter, simenao
+nome = prompt("Olá qual é o seu nome ?");
+peso = prompt("Qual é o seu peso ?");
+altura = prompt("Qual é a sua altura ?");
+datainter = prompt("Qual foi a data da sua internação ?");
+simenao = prompt("Você já foi internado ? ")
+
+alert(nome + " seu peso é " + peso + "\n sua altura é  " + altura + " \nsua da de internação é " + datainter + " \ninternado ? " + simenao )
+
+
+/*Indique o tipo de variável dos dados abaixo:
+
+nome: string
+sobrenome: string
+peso: number
+altura: number 
+dataDeInternacao:string
+diabético: string   */
